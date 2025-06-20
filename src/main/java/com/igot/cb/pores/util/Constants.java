1e537e0757ba7a6b3cb2e6e7b784d184e21bcf59
anilkumarkammalapalli <anilkumar.kammalapalli@tarento.com>
2025-06-20 14:25:15 +0530

    allowed state admin to create a framework

--- src/main/java/com/igot/cb/pores/util/Constants.java
+++ src/main/java/com/igot/cb/pores/util/Constants.java
@@ -375,7 +375,7 @@ public class Constants {
     public static final String ORGANISATION_ID = "organisationId";
     public static final String ORG_HIERARCHY_FRAMEWORK_ID_KEY = "orgHierarchyFrameworkId";
     public static final String ORG_HIERARCHY_FRAMEWORK_STATUS_KEY = "orgHierarchyFrameworkStatus";
-
+    public static final String STATE_ADMIN = "STATE_ADMIN";
 
 
 
